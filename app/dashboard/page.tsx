ef4aa640d5ee52102fe4e59c5dc7219202678e9c
mnove <novelli3000@gmail.com>
2025-05-02 17:20:24 +0200

    feat/add-dashboard-ex

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Dashboard from "@/components/layouts/dashboard/dashboard";
+
+export default function Page() {
+  return (
+    <div className="">
+      <Dashboard />
+    </div>
+  );
+}
+
+export const metadata = {
+  title: "Dashboard",
+  description: "This is the dashboard page. You can add your content here.",
+};
